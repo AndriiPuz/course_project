9d20b08daa0f46e0dabb63111b108c6b02dcc3a0
Andrii <puzyrandrii@gmail.com>
2023-02-28 19:37:33 +0200

    do something

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,12 @@ class Category < ApplicationRecord
 
     validates :name, uniqueness: true, presence: true
     validates :description, presence: true
+
+    def self.search(search)
+        if search
+            where(["name LIKE ? ", "%#{search}%"])
+        else
+            all
+        end
+    end
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,9 +1,22 @@
 class Operation < ApplicationRecord
   belongs_to :category
 
+  paginates_per 5
+
   enum otype: [:income, :outlay]
 
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :odate, presence: true
   validates :description, presence: true
+
+  scope :transfer_data, -> ( start_date, last_date, otype, category_id ) { where(odate: (start_date..last_date), otype: otype, category_id: category_id).group(:odate).sum(:amount) }
+  scope :category_data, -> ( start_date, last_date, otype ) { where(odate: (start_date..last_date), otype: otype).group(:category_id).sum(:amount) }
+
+  def self.search(search)
+    if search
+        joins(:category).where([" categories.name LIKE ? ", "%#{search}%"])
+    else
+        all
+    end
+  end
 end
